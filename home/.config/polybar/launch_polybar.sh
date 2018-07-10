8a28ef611dd26af7f2a297ca36168bd90765e1c1
EdyTheCow <github@edyyy.com>
2018-07-10 23:01:48 +0200

    adding configs

---
+++ home/.config/polybar/launch_polybar.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bars
+polybar middle &
+polybar right &
+polybar left &
+
+echo "Bars launched..."
